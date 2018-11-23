5ec8a0319b5f7a9cf1503c4596e3f6b66757ef4c
Harry Levick <harrylevick@hotmail.co.uk>
2018-11-23 17:52:44 +0000

    problem with return in FIB

--- csaAss2.s
+++ csaAss2.s
@@ -29,7 +29,7 @@ INPUT:
     
 SETUP:
     li   $a1, 0                 # a1 : i
-    sll  $a3, $t0, 2            # a3 : number of bytes needed 
+    sll  $a0, $t0, 2            # a0 : number of bytes needed 
     li   $v0, 9
     syscall                     # v0 : address of array space
     move $a2, $v0               # a2 : address of array space
@@ -47,7 +47,7 @@ LOOP:
     jal FIB
     # print fibonacci return
     move $a0, $v0
-    li   $v0, 4
+    li   $v0, 1
     syscall
     
     # preserve machine state
@@ -67,14 +67,47 @@ FIB:
     blez $a1, FIB0              # if (n <=0) return 0
     li   $t1, 1                 # t1 : 1
     beq  $a1, $t1, FIB1         # if (n == 1) return 1
-    addi $a2, $a2, $a1          # increment array pointer to get the position of a[n]
-    lw   $t2, ($a2)             # t2 : a[n]
-    bgtz $t2, 
+    add  $a2, $a2, $a1          # increment array pointer to get the position of a[n]
+    lw   $a0, ($a2)             # a0 : a[n]
+    bgtz $a0, FIB_MEMO
+    
+    # return the array pointer back to the start
+    sub  $a2, $a2, $a1          # decrement array pointer to get the starting position
+    addi $a1, $a1, -1
+    jal  FIB
+    
+    move $t2, $v0               # t2 : fib(n-1, memo)
+    
+    addi $a1, $a1, -1
+    jal  FIB
+    move $t3, $v0               # t3 : fib(n-2, memo)
+    
+    add  $t2, $t2, $t3          # t2 : fib(n-1, memo) + fib(n-2, memo)
+    
+    add  $a2, $a2, $a1          # increment array pointer to get to position of a[n]
+    sw   $t2, ($a2)             # store fib(n-1, memo) + fib(n-2, memo) at a[n]
+    sub  $a2, $a2, $a1          # decrement array pointer to get the starting position
+    
+    move $a0, $t2               # a0 : fib(n-1, memo) + fib(n-2, memo)
+    jal  FIB_MEMO
+    jr   $ra
+    
+    
     
     
 FIB0:
+    li   $v0, 0
+    jr   $ra
 
 FIB1:
+    li   $v0, 1
+    jr   $ra
+
+# @p : a0 : memo[n]
+FIB_MEMO:
+    move $v0, $a0
+    jr   $ra
+    
 
 FIB2:
 
